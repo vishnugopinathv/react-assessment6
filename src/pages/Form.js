884af49c22f9828f6d2a961f465cdd6c2a3826b1
vishnugopinathv <vishnugopinathv@gmail.com>
2022-05-30 18:55:08 +0530

    back button

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,53 +1,78 @@
-import React, { useState } from 'react'
-import Layout from './Layout';
-import { useInputContextConsumer } from '../Context/inputContext';
+import React, { useState, useEffect} from "react";
+import Layout from "./Layout";
+import { useInputContextConsumer } from "../Context/inputContext";
+import { useNavigate, useLocation } from "react-router-dom";
 const Form = () => {
-    const[userData,setUserData]=useState({
-      name:'',
-      age:'',
-      course:'',
-      batch:''
-    })
-    const{input,setInput}=useInputContextConsumer();
-    const changeHandler=(e)=>{
-        setUserData({...userData,[e.target.name]:e.target.value})
-    }
-    const submitHandler=(e)=>{
-      e.preventDefault();
-      setInput([...input,userData]); 
-      setUserData({
-        name:"",
-        age:"",
-        course:"",
-        batch:""
-      })
-    }
-    console.log(input);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    name: "",
+    age: "",
+    course: "",
+    batch: "",
+  });
+  const { input, setInput } = useInputContextConsumer();
+  const changeHandler = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setInput([...input, userData]);
+    setUserData({
+      name: "",
+      age: "",
+      course: "",
+      batch: "",
+    });
+    navigate(-1);
+  };
+  console.log(input);
+
   return (
     <Layout>
       <div className="form__container">
-          <form action="" onSubmit={submitHandler}>
-              <section className='input__item'>
-                <label htmlFor="">Name:</label>
-                <input type="text"name='name' onChange={changeHandler} value={userData.name}/>
-              </section>
-              <section className='input__item'>
-                <label htmlFor="">Age:</label>
-                <input type="text" name='age' onChange={changeHandler} value={userData.age}/>
-              </section>
-              <section className='input__item'>
-                <label htmlFor="">Course:</label>
-                <input type="text" name='course' onChange={changeHandler} value={userData.course}/>
-              </section>
-              <section className='input__item'>
-                <label htmlFor="">Batch:</label>
-                <input type="text" name='batch' onChange={changeHandler} value={userData.batch}/>
-              </section>
-              <input type="submit" />
-          </form>
+        <form action="" onSubmit={submitHandler}>
+          <section className="input__item">
+            <label htmlFor="">Name:</label>
+            <input
+              type="text"
+              name="name"
+              onChange={changeHandler}
+              value={userData.name}
+            />
+          </section>
+          <section className="input__item">
+            <label htmlFor="">Age:</label>
+            <input
+              type="text"
+              name="age"
+              onChange={changeHandler}
+              value={userData.age}
+            />
+          </section>
+          <section className="input__item">
+            <label htmlFor="">Course:</label>
+            <input
+              type="text"
+              name="course"
+              onChange={changeHandler}
+              value={userData.course}
+            />
+          </section>
+          <section className="input__item">
+            <label htmlFor="">Batch:</label>
+            <input
+              type="text"
+              name="batch"
+              onChange={changeHandler}
+              value={userData.batch}
+            />
+          </section>
+          <input type="submit" />
+        </form>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
--- src/pages/Students.js
+++ src/pages/Students.js
@@ -1,45 +1,56 @@
-import React from 'react'
-import Layout from './Layout'
-import {Link} from "react-router-dom"
-import { useInputContextConsumer } from '../Context/inputContext'
+import React from "react";
+import Layout from "./Layout";
+import { Link } from "react-router-dom";
+import { useInputContextConsumer } from "../Context/inputContext";
 function Students() {
-    const{input}=useInputContextConsumer();
+  const { input } = useInputContextConsumer();
   return (
     <Layout>
-        <div className="table__container">
+      <div className="table__container">
         <div className="student__page__head">
-            <h1>Students details</h1>
-            <Link to='/form'><button className='add__btn'>Add New Student</button></Link>
+          <h1>Students details</h1>
+          <Link to="/form">
+            <button className="add__btn">Add New Student</button>
+          </Link>
         </div>
         <table>
-            <tbody>
+          <tbody>
             <tr>
-                 <th>Name</th>
-                <th>Age</th>
-                 <th>Course</th>
-                  <th>Batch</th>
-                    <th>Change</th>
-           </tr>
-                {
-                    input && input.map(v=>{
-                        return(
-                            <tr>
-                            <td>{v.name}</td>
-                            <td>{v.age}</td>
-                            <td>{v.course}</td>
-                            <td>{v.batch}</td>
-                            <td><Link to='/form'>Edit</Link></td>
-                            </tr>
-                        )
-                    })
-                   
-                
-                }
-            </tbody>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Course</th>
+              <th>Batch</th>
+              <th>Change</th>
+            </tr>
+            {input &&
+              input.map((v,index) => {
+                return (
+                  <tr key={index}>
+                    <td>{v.name}</td>
+                    <td>{v.age}</td>
+                    <td>{v.course}</td>
+                    <td>{v.batch}</td>
+                    <td>
+                      <Link
+                        state={{
+                          name: v.name,
+                          age: v.age,
+                          course: v.course,
+                          batch: v.batch,
+                        }}
+                        to="/form"
+                      >
+                        Edit
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
         </table>
-        </div>
+      </div>
     </Layout>
-  )
+  );
 }
 
-export default Students
+export default Students;
